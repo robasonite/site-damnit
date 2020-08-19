ec4bd71d66183da9dd58f51c4cd2ad19447db89a
robasonite <robert@robasonite.com>
2020-08-19 18:34:30 -0400

    Moved site configuration to its own file.

---
+++ CONFIG.py
@@ -0,0 +1,11 @@
+# This is the configuration of your website in the form of a Python dictionary.
+# Keep this file separate from Site Damnit itself (damnit.py). It makes
+# updating easier.
+SITE_VARS = {}
+SITE_VARS['siteName'] = "Your site name here"
+SITE_VARS['siteAuthor'] = "Your Name Here"
+SITE_VARS['siteGenLunrJson'] = False
+SITE_VARS['siteDescription'] = "Enter a default description here."
+SITE_VARS['siteDefaultKeywords'] = "default, key, words, here"
+SITE_VARS['siteRoot'] = '/'
+
