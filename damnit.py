e40fa9f9aec23be92784b85709e4e2f7b9289ce6
Robert Kight <robert@robasonite.com>
2021-05-06 21:28:43 -0400

    Added more copyright stuff

--- damnit.py
+++ damnit.py
@@ -4,8 +4,21 @@
 #
 # Copyright (C) 2020 Robert Kight.
 #
-# This program is free and open source software. Read LICENSE for details.
-#
+# This program is free and open source software released under the GNU GPLv3.
+# See COPYING for full license text.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import pystache # external
 import commonmark # external
 import yaml # external? Fedora 30 seemed to install it by default, but not Ubuntu
